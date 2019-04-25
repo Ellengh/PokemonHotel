9a95a0cf9f9566fde279681113c8763c437f0390
StephenGregan <grego@live.ie>
2019-04-25 20:04:40 +0100

    new

--- src/pokemonhotel/SetUp.java
+++ src/pokemonhotel/SetUp.java
@@ -21,7 +21,7 @@ public class SetUp
 
     DataStore dataStore = new DataStore();
 
-    public ArrayList<Pokemon> returnedPokemon(ArrayList<Pokemon> inmates, int amount)
+    public ArrayList<Pokemon> returnedPokemon(ArrayList<Pokemon> pokemon, int amount)
     {
         for (int i = 0; i < amount; i++)
         {
@@ -31,25 +31,46 @@ public class SetUp
                 case 0:
                     LandPokemon landPokemon = new LandPokemon();
                     landPokemon.setName(dataStore.getPokemonNames().get(rGen.nextInt(dataStore.getPokemonNames().size())));
+                    landPokemon.setExpertLevel(choice);
+                    landPokemon.setGenericStamp(choice);
+                    landPokemon.setResistanceRating(choice);
+                    landPokemon.setRoomNumber(choice);
+                    landPokemon.setSpecialAttackRating(choice);
+                    landPokemon.setSpeedRating(choice);
+                    landPokemon.setType(landPokemon);
                     System.out.println("Random pokemon: " + landPokemon.getName());
-                    inmates.add(landPokemon);
+                    pokemon.add(landPokemon);
                     break;
                 case 1:
                     AirPokemon airPokemon = new AirPokemon();
                     airPokemon.setName(dataStore.getPokemonNames().get(rGen.nextInt(dataStore.getPokemonNames().size())));
+                    airPokemon.setExpertLevel(choice);
+                    airPokemon.setGenericStamp(choice);
+                    airPokemon.setResistanceRating(choice);
+                    airPokemon.setRoomNumber(choice);
+                    airPokemon.setSpecialAttackRating(choice);
+                    airPokemon.setSpeedRating(choice);
+                    airPokemon.setType(airPokemon);
                     System.out.println("Random pokemon: " + airPokemon.getName());
-                    inmates.add(airPokemon);
+                    pokemon.add(airPokemon);
                     break;
                 case 2:
                     WaterPokemon waterPokemon = new WaterPokemon();
                     waterPokemon.setName(dataStore.getPokemonNames().get(rGen.nextInt(dataStore.getPokemonNames().size())));
+                    waterPokemon.setExpertLevel(choice);
+                    waterPokemon.setGenericStamp(choice);
+                    waterPokemon.setResistanceRating(choice);
+                    waterPokemon.setRoomNumber(choice);
+                    waterPokemon.setSpecialAttackRating(choice);
+                    waterPokemon.setSpeedRating(choice);
+                    waterPokemon.setType(waterPokemon);
                     System.out.println("Random pokemon: " + waterPokemon.getName());
-                    inmates.add(waterPokemon);
+                    pokemon.add(waterPokemon);
                     break;
                 default:
                     System.out.println("Something went wrong!!!");
             }
         }
-        return inmates;
+        return pokemon;
     }
 }
